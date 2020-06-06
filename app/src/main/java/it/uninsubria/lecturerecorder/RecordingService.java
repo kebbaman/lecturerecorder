b4a83bd255387b98feeb0f544b79ad1718e8a2d6
kebba <kebbaman@gmail.com>
2020-06-06 21:22:21 +0200

    Creato recordingService per l'effettiva registrazione e l'effettivo salvataggio dei file Audio.

--- app/src/main/java/it/uninsubria/lecturerecorder/RecordingService.java
+++ app/src/main/java/it/uninsubria/lecturerecorder/RecordingService.java
@@ -1,4 +1,86 @@
 package it.uninsubria.lecturerecorder;
 
-public class RecordingService {
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import java.io.File;
+import java.io.IOException;
+
+public class RecordingService extends Service
+{
+    MediaRecorder mediaRecorder;
+    long startingTimeMillis = 0;
+    long elapsedMillis = 0;
+
+    File file;
+    String fileName;
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startRecording();
+        return START_STICKY;
+        //return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void startRecording()
+    {
+        Long tsLong = System.currentTimeMillis()/1000;
+        String ts = tsLong.toString();
+        fileName = "audio_"+ts;
+        file = new File(Environment.getExternalStorageDirectory()+ "/Recordings/"+fileName+".mp3");
+
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mediaRecorder.setOutputFile(file.getAbsolutePath());
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mediaRecorder.setAudioChannels(1);
+
+        try {
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+            startingTimeMillis = System.currentTimeMillis();
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void stopRecording()
+    {
+        mediaRecorder.stop();
+        elapsedMillis = System.currentTimeMillis() - startingTimeMillis;
+        mediaRecorder.release();
+        Toast.makeText(getApplicationContext(),"Recording saved: "+file.getAbsolutePath(),Toast.LENGTH_LONG).show();
+
+        //aggiunta a database sqlite
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        if(mediaRecorder != null)
+            stopRecording();
+        super.onDestroy();
+    }
 }
